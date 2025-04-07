24e09a526c2d5a70b040aa571d18eb2e68480968
vgakhar <vgakhar@inscopix.com>
2025-04-07 14:30:30 +0530

    added new methods

--- src/utils/streaming.js
+++ src/utils/streaming.js
@@ -94,6 +94,17 @@ export function subscribeStreaming(janus, opaqueId, callback) {
 								Janus.debug(jsep);
 								var body = { "request": "start" };
 								streaming.send({"message": body, "jsep": jsep});
+								const pc = streaming.webrtcStuff.pc;
+								console.log("Initial ICE state from creat answer method:", pc.iceConnectionState);
+
+								pc.oniceconnectionstatechange = () => {
+								console.log("ICE connection state from create answer method:", pc.iceConnectionState);
+
+								if (pc.iceConnectionState === "disconnected" || pc.iceConnectionState === "failed") {
+									console.warn("ICE connection lost from creat answer method:! Reconnect or alert the user.");
+									// You can trigger recovery/reconnect logic here
+								}
+								};
 							},
 							error: function(error) {
 								Janus.error("WebRTC error:", error);
