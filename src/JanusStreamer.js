9d1c9f6ba452015a271f473564837b0e281f5784
Aaryan Saxena <aaryan.saxena@pragmaapps.com>
2025-08-27 03:07:47 -0700

    Added build according to react 19

--- src/JanusStreamer.js
+++ src/JanusStreamer.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 import Janus from './utils/janus';
 import { subscribeStreaming, startStream } from './utils/streaming';
 import JanusStreamPlayer from './JanusStreamPlayer';
-import { Video } from 'video-react';
+// import { Video } from 'video-react';
 
 const JanusStreamer = React.forwardRef((
     {
@@ -64,7 +64,7 @@ const JanusStreamer = React.forwardRef((
 
     const handlePlayEvent = (e) => {
         console.log("[JanusStreamer] Live Stream Playing", e);
-        videoArea.current !== null && videoArea.current.video.video.play();
+        videoArea.current !== null && videoArea.current.play();
         streamIsLive(true);
     }
     const streamingCallback = (_streaming, eventType, data) => {
@@ -73,8 +73,9 @@ const JanusStreamer = React.forwardRef((
             mystream = data;
 
             console.log("[Attaching stream to the video element:]", videoArea);
-            const videoPlayer = videoArea.current.video.video
+            const videoPlayer = videoArea.current;
             Janus.attachMediaStream(videoPlayer, mystream);
+            videoPlayer.play();
             videoPlayer.addEventListener('error', handleErrorVideo);
             videoPlayer.addEventListener('play', handlePlayEvent);
             if (_streaming.webrtcStuff.pc.iceConnectionState !== "completed" &&
